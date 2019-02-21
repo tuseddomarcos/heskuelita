dfedfd6874dd89e70ee02e5971e457cb51f7f8f0
Marcos <marcosincacorreo@hotmail.com>
2019-02-21 10:44:00 -0300

    Clase-02/21

--- contenidoPGAadmin/Course.Data.sql
+++ contenidoPGAadmin/Course.Data.sql
@@ -1,3 +1,8 @@
-INSERT INTO public.course(
-	name, description, duration, limit_person)
-	VALUES ('OOP', 'Objecr Orient Programming', 15, 20);
+INSERT INTO public.course(name, description, duration, limit_person)
+	VALUES ('OOP', 'Object Orient Programming', 15, 20);
+
+INSERT INTO public.course(name, description, duration, limit_person)
+	VALUES ('Java', 'Java Language ', 30, 15);
+	
+INSERT INTO public.course(name, description, duration, limit_person)
+	VALUES ('SQL', 'mySQL and tables concepts', 10, 30);
