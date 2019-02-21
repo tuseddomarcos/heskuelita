c39b87927fab42d4f71b2b7e0db370efdfb89f4c
Brpalacio <bpalacio@arbal009733.corp.capgemini.com>
2019-02-21 11:27:14 -0300

    PROYECTO1

---
+++ core/src/main/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	public static void main (String [ ] args) {
+		Student s1 = new Student();
+		Student s2 = new Student();
+		Student s3 = new Student();
+		Student s4 = new Student();
+		
+		
+	}
+}
