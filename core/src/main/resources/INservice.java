dec6ecf5fde4af0b5d481970cf7453918352d550
andres2405 <petrizzoes@hotmail.com>
2019-02-21 11:55:49 -0800

    Service

---
+++ core/src/main/resources/INservice.java
@@ -0,0 +1,14 @@
+
+public interface INservice {
+	public void SinInStudent();
+	public Student Read(Student student);
+	public void Update(Student student);
+	public void Delete(Student student);
+	
+	public void SinInClassroom();
+	public Student Read(Classroom classroom);
+	public void Update(Classroom classroom);
+	public void Delete(Classroom classroom);
+	
+	
+}
