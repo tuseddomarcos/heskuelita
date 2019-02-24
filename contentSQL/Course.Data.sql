ce61035c2c67c50cf06724d51a7db8b0990757bb
Marcos Inca <marcosincacorreo@hotmail.com>
2019-02-24 14:43:37 -0300

    2402

--- contentSQL/Course.Data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO public.course(name, description, duration, limit_person)
-	VALUES ('OOP', 'Object Orient Programming', 15, 20);
-
-INSERT INTO public.course(name, description, duration, limit_person)
-	VALUES ('Java', 'Java Language ', 30, 15);
-	
-INSERT INTO public.course(name, description, duration, limit_person)
-	VALUES ('SQL', 'mySQL and tables concepts', 10, 30);
