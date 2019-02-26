56b45bf5e8065e1cd122bd991b4e7a1ea217c731
Martina <martinacesca@gmail.com>
2019-02-26 10:00:36 -0800

    Adding class diagram

---
+++ core/src/main/java/com/capgemini/heskuelita/beans/Test.java
@@ -0,0 +1,37 @@
+package com.capgemini.heskuelita.beans;
+import com.capgemini.heskuelita.beans.*;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println ("Igrese su nombre:");
+		Scanner in = new Scanner (System.in);
+		System.out.println ("Su nombre es: " + in.next());
+		
+		
+	}
+	/*
+		List<Student> a = new ArrayList<Student>();
+		a = Arrays.asList{
+			new Student();
+			new Student("Lucia","Gomez", 2000-02-02,"DNI",34567890,48256789,"F","Capital","Corrientes 1233",2345);
+		}
+		
+		List<Student> 
+	}
+	
+	public static 
+		student.forEach ( e-> {if (endWith("ez")) {emails.add (e); System.out.println(e);
+		}
+	}
+*/
+}
+
+
