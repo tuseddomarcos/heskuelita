bd8ec0b3ddc505a2ff6975e5684044cb94d9f6c3
Gonzalo <mauricio_gzo@hotmail.com>
2019-02-27 11:43:22 -0300

    Se agrega carpeta data

---
+++ data/src/main/resources/com/capgemini/heskuelita/data/DataBaseManager.java
@@ -0,0 +1,13 @@
+package com.capgemini.heskuelita.data;
+import java.sql.*;
+
+
+public class DataBaseManager {
+
+	public static void main (String [] args) {
+		
+		Connector connector = new Connect
+		
+	}
+	
+}
